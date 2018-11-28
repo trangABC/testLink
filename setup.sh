574239c1f105a9619e7ea58f797954e74d5455ad
TrangABC <trang@email.com>
2018-11-28 14:24:58 +0700

    UPDATE: setup.sh

--- setup.sh
+++ setup.sh
@@ -80,30 +80,45 @@ usage="USAGE: ${SCRIPT_NAME} [-h]
     
     Script to set up and run ShareLedger Masternode
 
-1. To setup Masternode, run the following command:
+    Global param:
+        -h  show this help text
+
+To setup & run your Masternode, please follow these steps:
+
+1. Generate an account and essential configuration files
         ${SCRIPT_NAME} -m moniker -t tokens [-c dir] [-p p2p_port] [-r rpc_port] [-w website] [-d details]
 
     where:
-        -h  show this help text
-        -m  Moniker name. Your Masternode unique name.
-        -t  number of SHR tokens to be staked.
+        -m  Moniker name. Your Masternode unique name. REQUIRED
         -c  Configuration directory. (Optional. Default: ${CONFIGDIR})
         -p  P2P port. Port to open for other Masternode to listen to. (Optional. Default: 46656)
         -r  RPC port. Port for clients to connect to. ( Optional. Default: 46657 )
+        
+    Example:
+            ${SCRIPT_NAME} -m sharering
+
+2. Collect the address and email us at <TO-DO>@sharering.network
+        ${SCRIPT_NAME} address
+
+3. Register your node as a Masternode
+        ${SCRIPT_NAME} register [-c dir]
+
+    where:
+        -t  number of SHR tokens to be staked. REQUIRED
+        -c  Configuration directory. (Optional. Default: ${CONFIGDIR})
         -w  Masternode website. (Optional. Default: \"sharering.network\")
         -d  Maternode details. (Optional. Default: \"ShareLedger Masternode\")
 
     Example:
-            ${SCRIPT_NAME} -m sharering -t 100
+        ${SCRIPT_NAME} -t 10
 
-2. To run Masternode, execute:
+4. To run the Masternode, execute:
         ${SCRIPT_NAME} run [-c dir]
 
     where:
-        -h  show this help text
         -c  Configuration directory. (Optional. Default: ${CONFIGDIR})
-
-    Please use the same configuration dir (-c) with the one in the setup phase if you specified different one.
+        
+NOTE:  Please use the same configuration dir (-c) with the one in the setup phase if you specified a different one.
 "
 
 
@@ -113,16 +128,10 @@ usage="USAGE: ${SCRIPT_NAME} [-h]
 init_args () {
     local OPTIND OPTARG
 
-    while getopts 'hm:t:c:p:r:w:d:' option; do
+    while getopts 'hm:c:p:r:' option; do
       case "$option" in
         h) echo "$usage"
            exit
-           ;;
-        m) MONIKER=$OPTARG
-           #echo "MONIKER: ${MONIKER}"
-           ;;
-        t) TOKENS=$OPTARG
-           #echo "TOKEN: ${TOKENS}"
            ;;
         c) CONFIGDIR=$OPTARG
             #echo "CONFIGDIR: ${CONFIGDIR}"
@@ -133,12 +142,8 @@ init_args () {
         r) RPC_PORT=$OPTARG
             #echo "RPC_PORT: ${RPC_PORT}"
             ;;
-        w) WEBSITE=$OPTARG
-            #echo "WEBSITE: ${WEBSITE}"
-            ;;
-        d) DETAILS=$OPTARG
-            #echo "DETAILS: ${DETAILS}"
-            ;;
+        m) MONIKER=$OPTARG
+           ;;
         :) printf "missing argument for -%s\n\n" "$OPTARG" >&2
            echo "$usage" >&2
            exit 1
@@ -159,7 +164,6 @@ init_args () {
         exit 1
     fi
 }
-
 
 run_args () {
     local OPTIND OPTARGS
@@ -183,6 +187,44 @@ run_args () {
     shift $((OPTIND - 1))
 
 }
+
+register_args () {
+    local OPTIND OPTARG
+
+    while getopts 'ht:c:w:d:' option; do
+      case "$option" in
+        h) echo "$usage"
+           exit
+           ;;
+        t) TOKENS=$OPTARG
+           ;;
+        c) CONFIGDIR=$OPTARG
+            ;;
+        w) WEBSITE=$OPTARG
+            ;;
+        d) DETAILS=$OPTARG
+            ;;
+        :) printf "missing argument for -%s\n\n" "$OPTARG" >&2
+           echo "$usage" >&2
+           exit 1
+           ;;
+       \?) printf "illegal option: -%s\n\n" "$OPTARG" >&2
+           echo "$usage" >&2
+           exit 1
+           ;;
+      esac
+    done
+    shift $((OPTIND - 1))
+
+
+    if [[ -z ${MONIKER} || ${TOKENS} == 0 ]]; then
+        echo -e "-m (Moniker) and -t (tokens) are required"
+        echo
+        echo "$usage" >&2
+        exit 1
+    fi
+}
+
 
 prepare () {
     # Check wget/curl
@@ -288,14 +330,28 @@ init () {
     # download genesis
     download "${GENESIS_URL}" "${CONFIGDIR}/config/genesis.json" || exit 1
 
-    # register Masternode
-    "${SHARELEDGER}" register_masternode --home "${CONFIGDIR}" --moniker "${MONIKER}" --tokens "${TOKENS}" --website "${WEBSITE}" --details "${DETAILS}"
-   
+  
 }
 
 run () {
     run_args "$@"
     "${SHARELEDGER}" node
+}
+
+register () {
+    register_args "$@"
+    # register Masternode
+    "${SHARELEDGER}" register_masternode --home "${CONFIGDIR}"  --tokens "${TOKENS}" --website "${WEBSITE}" --details "${DETAILS}"
+}
+
+show_address () {
+    if [[ ! -f ${SHARELEDGER} ]]; then
+        echo -e "Executable *shareledger* not found."
+        echo -e "Please init first."
+        echo "$usage"
+    else
+        "${SHARELEDGER}" show_priv_key
+    fi
 }
 
 main () {
@@ -317,13 +373,17 @@ main () {
             ;;
         address)
             shift
-            if [[ ! -f ${SHARELEDGER} ]]; then
-                echo -e "Executable *shareledger* not found."
-                echo -e "Please init first."
-                echo "$usage"
-            else 
-                "${SHARELEDGER}" show_priv_key
-            fi
+            show_address
+            exit 0
+            ;;
+        register)
+            shift
+            register "$@"
+            exit 0
+            ;;
+        -h|--help)
+            echo "$usage"
+            exit 0
             ;;
         *)
             echo -e "Unknown command $1"
